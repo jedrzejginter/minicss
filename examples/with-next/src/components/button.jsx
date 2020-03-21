c322f5eceec7637195bb727516b7e50f0554353e
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-03-21 17:10:08 +0100

    something is still not right with caching

--- examples/with-next/src/components/button.jsx
+++ examples/with-next/src/components/button.jsx
@@ -1,3 +1,3 @@
 export default function Button() {
-  return <button className="Pos(r) Ap(n) Bd(2,s,red) Bdrs(5) Ff(i) Fz(14) Fw(b) P(10) Bgc(#e2b207) Bgc(red):h">Click me!</button>
+  return <button className="C(brown) Pos(r) Ap(n) Bd(2,s,red) Bdrs(5) Ff(i) Fz(14) Fw(b) P(10) Bgc(#e2b207) Bgc(red):h">Click me!</button>
 }
