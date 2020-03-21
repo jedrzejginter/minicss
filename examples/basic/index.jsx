1bc4f0a9b27054b1471f2fec057f490993c17e68
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-03-21 12:31:53 +0100

    Update appearance properties

--- examples/basic/index.jsx
+++ examples/basic/index.jsx
@@ -1,6 +1,6 @@
 export function MyComponent() {
   return (
-    <div className="Op(75%) Op(90%):h C(red) M(-10) W([100%-5rem]x3) Px(20)">
+    <div className="Ap(n) Op(75%) Op(90%):h C(red) M(-10) W([100%-5rem]x3) Px(20)">
       <span className="W(100)">
         Hello, world!
       </span>
