5be0eec151e99c115e7f91ca53a3811889d1cdda
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-03-21 13:17:14 +0100

    Support hex with opacity

--- examples/basic/other.jsx
+++ examples/basic/other.jsx
@@ -1,8 +1,8 @@
 export function MyOtherComponent() {
   return (
-    <div className="Bd(1,s,0x019) T(10px) W(40) P(10,20,30) P(20):h">
+    <div className="C(0x0a6a8c.75) sm:T(3) Z(dropdown) Ord(1) Fxg(1) Fz(12) Fz(14):h Bd(1,s,0x019) T(10px) W(40) P(10,20,30) P(20):h">
       <span className="Ai(c)">
-        Hello, other!
+        Hello, other!!
       </span>
     </div>
   );
