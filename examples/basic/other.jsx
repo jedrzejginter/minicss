4d8ac36c8ff069abb9426e925539c51c05d1e95a
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-03-21 12:28:27 +0100

    support multiple properties

---
+++ examples/basic/other.jsx
@@ -0,0 +1,9 @@
+export function MyOtherComponent() {
+  return (
+    <div className="Bd(1,s,0x019) T(10px) W(40) P(10,20,30) P(20):h">
+      <span className="Ai(c)">
+        Hello, other!
+      </span>
+    </div>
+  );
+}
