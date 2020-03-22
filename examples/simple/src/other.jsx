3bcde2afc3a43533d1e13583d69bbdaa0b666ad6
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-03-22 15:58:53 +0100

    Add verbose mode

--- examples/simple/src/other.jsx
+++ examples/simple/src/other.jsx
@@ -1,7 +1,7 @@
 export function MyOtherComponent() {
   return (
     <div className="C(0x0a6a8c.75) sm:T(3) Z(dropdown) Ord(1) Fxg(1) Fz(12) Fz(14):h Bd(1,s,0x019) T(10px) W(40) P(10,20,30) P(20):h">
-      <span className="Ai(c)">
+      <span className="C(green)">
         Hello, other!!
       </span>
     </div>
