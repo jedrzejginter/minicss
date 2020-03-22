01a4348fc4813486534316d7046df9cb9dc588eb
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-03-22 15:22:22 +0100

    rename things, read config from package.json

--- examples/with-next/src/pages/index.js
+++ examples/with-next/src/pages/index.js
@@ -1,8 +1,16 @@
+import Head from 'next/head';
 import Button from '../components/button';
 
 export default function Index() {
-  return <div className="C(yellowgreen) Bgc(orange):a Px(100) W(120)">
-    Hello!!<br />
-    <Button />
-  </div>
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="/css/styles.css" />
+      </Head>
+      <div className="C(#77d) Bgc(orange):a Px(20) W(120)">
+        <span className="C(#fff) Bgc(grey)">Hello</span>
+        <Button />
+      </div>
+    </>
+  );
 }
